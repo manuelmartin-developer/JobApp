5e053b8bcb1ac1cf103e6cfe6101a31082159883
Fabrizio <fabacasa@gmail.com>
2021-09-02 23:11:35 +0200

    NOSQL DB-> Job schema created

--- models/jobSchema.js
+++ models/jobSchema.js
@@ -1,24 +1,28 @@
 const mongoose = require('../utils/nosql_db')
 
 const jobSchema = new mongoose.Schema({
-    id: {
+    title: {
         type: String,
-        required: true,
     },
-    favorites: Array,
-
-
+    company: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    date: { 
+        type: Date, 
+        default: Date.now 
+    },
+    salary: {
+        type: Number,
+    },
 })
 
 module.exports = mongoose.model('Job', jobSchema, 'JobOffers')
-
-
-/*
-title
-company
-location
-image
-description
-
-
-*/

464ebbb16cc3f9da38ad934fd21d1626fc5ae6c2
manuelmartin-developer <hola@manuelmartin.name>
2021-09-06 23:44:03 +0200

    Refactor

--- utils/create_job.js
+++ utils/create_job.js
@@ -1,33 +1,91 @@
 const input_title = document.querySelector('#input_title');
 const input_company = document.querySelector('#input_company');
 const input_location = document.querySelector('#input_location');
-const input_date= document.querySelector('#input_date');
+const input_date = document.querySelector('#input_date');
 const input_image = document.querySelector('#input_image');
 const input_url = document.querySelector('#input_url');
-const list_jobs = document.querySelector('.list_jobs');
+const cards = document.querySelector('.cards');
 
 const btn_create = document.querySelector('#btn_create');
 
-// Este método carga todos los jobs de la base de datos, haciendo
-// un fetch a la api. Una vez elaborado el método para pintar cada tarjeta
-// habrá que refactorizarlo. 
+//! event.preventdefault() ya no funciona, ya que no usamos
+//! un formulario, sino un botón. Hay que ver la manera de
+//! evitar que se lance el post sin tener los datos en los input.
+// Renderizamos en la vista los trabajos que están
+// alojados en la BD
 
 (async () => {
     const response = await fetch('/api/ads');
     const jobs = await response.json();
 
-    for (job of jobs) {
-        //! REFACTORIZAR
-        //? Aquí irá el método paintCard que pintará una tarjeta por cada elemento de la DB
-        const card = document.createElement('div');
-        card.setAttribute('class', 'detail-card')
-        card.innerText = job.title;
-        list_jobs.appendChild(card);
+    // Pintamos cada una de las tarjetas
+    jobs.forEach(job => {
+        paintCard(job);
+
+    })
+    // Ańadimos los botones de editar y borrar
+    const cards = document.querySelectorAll('.card')
+
+    for (let card of cards) {
+
+        const url = card.querySelector('a').href;
+        const editButton = document.createElement('button');
+        editButton.setAttribute("name", url)
+        const iconEdit = document.createElement('i');
+        iconEdit.setAttribute('class', 'far fa-edit');
+        editButton.appendChild(iconEdit);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute("name", url)
+        const iconDelete = document.createElement('i');
+        iconDelete.setAttribute('class', 'far fa-trash-alt');
+        deleteButton.appendChild(iconDelete);
+
+        card.appendChild(editButton);
+        card.appendChild(deleteButton);
+
+        editButton.addEventListener('click', () => {
+            // Hay que hacer un alert con un formulario?
+            // en el que puedas editar el trabajo.
+        });
+
+        deleteButton.addEventListener('click', () => {
+
+            // Alert de confirmación de borrado
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: "Esta acción no podrá deshacerse",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: '¡Sí, bórralo!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch('/api/ads', {
+                        method: 'DELETE',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            url: deleteButton.name
+                        })
+                    });
+                    Swal.fire(
+                        'Borrado!',
+                        'El trabajo ha sido borrado de la DB',
+                        'success'
+                    )
+                }
+            })
+        });
     }
+
 })()
 
 btn_create.addEventListener('click', (event) => {
-    event.preventDefault()
     // Con este listener simulamos manualmente un método POST -> recogemos los values de cada input -> se igualan al esquema new Job en "postJob" -> lo guarda a su vez en la base de datos -> se transforma en json y lo devuelve
     let getData = async () => {
         const response = await fetch('/api/ads', {
@@ -48,13 +106,18 @@ btn_create.addEventListener('click', (event) => {
         const data = await response.json();
         return data;
     }
-    let renderData = async () => {
-        const data = await getData()
-        // Aquí se crea la tarjeta del job creado (con appendChild)
-        //! VER demo clase 20 del 31/08 demo-client/js/utils/paintData
-        list_jobs.innerHTML = data
-        console.log(data)
-    }
-    renderData()
+    // Renderizamos el nuevo empleo en la vista
+    (async () => {
+        const newJob = await getData()
+
+        // Lanzamos un alert cuando se crea el nuevo trabajo
+        Swal.fire(
+            `${newJob.jobTitle}`,
+            'añadido a la BD',
+            'success'
+        )
+        // Pintamos el nuevo trabajo
+        paintCard(newJob);
+    })()
 
 })
